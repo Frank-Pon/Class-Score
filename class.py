8eb31925efa7d823bb9be840a2fc233797f25d6e
Frank-Pon <cry133216@gmail.com>
2025-04-30 15:23:33 +0800

    班級成績模擬練習

--- class.py
+++ class.py
@@ -39,10 +39,21 @@ def no_pass(students):
     # 帶入全班資料,回傳未通過人數及未通過名單 (tuple)
 
 
+def comment(student):
+    if student['avg'] >= 90:
+        return '優秀'
+    elif student['avg'] >= 70 and student['avg'] <= 89:
+        return '良好'
+    elif student['avg'] >= 60 and student['avg'] <= 69:
+        return '普通'
+    return '不及格'
+
+
 for student in students:  # 從全班裡面找到個人
     avg = student_avg(student)  # 取得個人平均成績
     student['avg'] = avg  # 將平均成績加入個人成績資料
     student['passed'] = get_passed(student)  # 將是否通過加入個人成績資料
+    student['comment'] = comment(student)  # 將評語加入個人成績資料
 
 sub_avg = class_avg(subjects)  # 取得班級各科目及其平均成績
 
@@ -55,10 +66,9 @@ for student in students:
 print('===========================')
 print('Avg:')
 for sub, avg in sub_avg.items():
-    print(f'Class {sub.capitalize()} Avg : {avg}')
+    print(f'Class {sub.capitalize()} avg : {avg}')
     # 輸出班級各科目平均
 print('===========================')
-print(
-    f'通  過  人  數 : {pass_stu(students)[0]} 人 , 名 單 : {pass_stu(students)[1]}')
-print(f'未 通 過 人 數 : {no_pass(students)[0]} 人 , 名 單 : {no_pass(students)[1]}')
+print(f'通  過  人  數 : {pass_stu(students)[0]}  名 單 : {pass_stu(students)[1]}')
+print(f'未 通 過 人 數 : {no_pass(students)[0]}  名 單 : {no_pass(students)[1]}')
 # 輸出通過及未通過人數及名單 (tuple形式,可以使用索引值取資料)
